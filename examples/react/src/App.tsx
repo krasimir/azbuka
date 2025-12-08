52791aa9cb871a62a1313446fe630e1b92e1ead0
Krasimir <krasimir.st.tsonev@gmail.com>
2025-12-08 09:16:19 +0200

    New release

--- examples/react/src/App.tsx
+++ examples/react/src/App.tsx
@@ -3,8 +3,7 @@ import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 import './forgecss.css';
-// import fx from '../../../packages/forgecss/fx'
-import fx from 'forgecss/fx'
+import fx from '../../../packages/forgecss/fx'
 
 function App() {
   const [count, setCount] = useState(0)
