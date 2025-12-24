4d49fa3cde6337d838d5d34c00b42f5ffaa94da9
Krasimir <krasimir.st.tsonev@gmail.com>
2025-12-24 12:09:39 +0200

    Working on the site

--- packages/site/src/App.tsx
+++ packages/site/src/App.tsx
@@ -72,18 +72,19 @@ function App() {
       <header>
         <div className={fx("maxw1000 mxauto p1 desktop:py3")}>
           <div className="flex-center gap1">
-            <img src="/forgecss.svg" width="100" height="100" />
+            <img src="/forgecss.svg" width="100" height="100" alt="ForgeCSS logo"/>
           </div>
         </div>
       </header>
       <section className={fx("hero py3 mobile:p1")}>
         <div className={fx("maxw800 mxauto grid2x1 gap2 mobile:b")}>
           <div>
-            <h1 className={fx("fz2 desktop:fz3")}>A compiler for utility classes.</h1>
-            <p className="fz15 mt3">
-              <span className="success">✔</span> CSS compiler that understands class syntax<br />
-              <span className="success">✔</span> It parses class strings, applies rules and structure, and compiles
-                them into CSS.
+            <h1 className={fx("fz2 mobile:mt1 desktop:fz3")}>A compiler for utility classes.</h1>
+            <p className={fx("fz15 mt1 desktop:mt2")}>
+              <span className="success">✔</span> Compiler that understands CSS class syntax
+              <br />
+              <span className="success">✔</span> It parses class strings, applies rules and structure, and compiles them
+              into CSS.
             </p>
             <p className="mt1">
               What it isn’t!
@@ -103,9 +104,20 @@ function App() {
                 <span className="warning">✖</span> Not a CSS-in-JS solution
               </small>
             </p>
+            <p className="mt1">
+              ForgeCSS gives you the freedom to create your own utilities and compile them into CSS.
+            </p>
           </div>
-          <div className={fx("mobile:mt3")}>
+          <div className={fx("mobile:mt2")}>
             <nav className="flex-col gap1">
+              <a href="#syntax" className="flex-center gap05">
+                <img src="/align-left.svg" width="20" />
+                Syntax
+              </a>
+              <a href="#api" className="flex-center gap05">
+                <img src="/sliders.svg" width="20" />
+                API
+              </a>
               <a href="#playground" className="flex-center gap05">
                 <img src="/code.svg" width="20" />
                 Playground
@@ -116,6 +128,9 @@ function App() {
               </a>
             </nav>
           </div>
+        </div>
+        <div style={{ maxWidth: "800px" }} className="mxauto mt3">
+          <img src="/forgecss-diagram.svg" className={fx("hero-image b mxauto")} style={{ maxWidth: "100%" }} alt="ForgeCSS how-it-works diagram" />
         </div>
       </section>
       <main className="black-bg" id="playground">
